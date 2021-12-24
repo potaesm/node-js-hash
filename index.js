4f5c4d32aec406ebf7abd1f4d95b9bb096f0b8d2
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2021-12-24 15:50:55 +0530

    Add HMAC_SHA256

--- index.js
+++ index.js
@@ -1,61 +1,23 @@
 const SHA256 = require('./SHA256');
 
-function HMAC_SHA256(data = '', key = '') {
-    let hashContext = key;
-    /** SHA256 Block Size */
-    const B = 64;
-    const I_PAD = parseInt('0x36', 16);
-    const O_PAD = parseInt('0x5c', 16);
-    /**
-     * If the key length is bigger than the buffer size B, apply the hash
-     * function to it first and use the result instead.
-     */
-    let keyLength = key.length;
-    if (keyLength > B) {
+function HMAC_SHA256(message = '', key = '') {
+    const BLOCK_SIZE = 64;
+    const I_PAD = 0x36;
+    const O_PAD = 0x5c;
+    if (key.length > BLOCK_SIZE) {
         key = SHA256(key);
-        hashContext = key;
-        keyLength = key.length;
+    } else {
+        while (key.length < BLOCK_SIZE) {
+            key += '0';
+        }
     }
-    /**
-     * (1) append zeros to the end of K to create a B byte string
-     *     (e.g., if K is of length 20 bytes and B=64, then K will be
-     *     appended with 44 zero bytes 0x00)
-     * (2) XOR (bitwise exclusive-OR) the B byte string computed in step
-     *     (1) with ipad
-     */
-    const kx = [];
-    for (let i = 0; i < keyLength; i++) kx[i] = I_PAD ^ key[i].charAt(0);
-    for (let i = keyLength; i < B; i++) kx[i] = I_PAD ^ 0;
-    /**
-     * (3) append the stream of data 'text' to the B byte string resulting
-     *     from step (2)
-     * (4) apply H to the stream generated in step (3)
-     */
-    for (let i = 0; i < kx.length; i++) {
-        hashContext += String.fromCharCode(kx[i]);
-    }
-    for (let i = 0; i < data.length; i++) {
-        hashContext += data[i];
-    }
-    /**
-     * (5) XOR (bitwise exclusive-OR) the B byte string computed in
-     *     step (1) with opad
-     *
-     * NOTE: The "kx" variable is reused.
-     */
-    for (let i = 0; i < keyLength; i++) kx[i] = O_PAD ^ key[i].charAt(0);
-    for (let i = keyLength; i < B; i++) kx[i] = O_PAD ^ 0;
-    /**
-     * (6) append the H result from step (4) to the B byte string
-     *     resulting from step (5)
-     * (7) apply H to the stream generated in step (6) and output
-     *     the result
-     */
-    for (let i = 0; i < kx.length; i++) {
-        hashContext += String.fromCharCode(kx[i]);
-    }
-    console.log(hashContext);
-    return SHA256(hashContext);
+    const I_KEY_PAD = [];
+    for (let i = 0; i < BLOCK_SIZE; i++) I_KEY_PAD[i] = key[i].charAt(0) ^ I_PAD;
+    const hashSum1 = SHA256(I_KEY_PAD.map(i => String.fromCharCode(i)).join('') + message);
+    const O_KEY_PAD = [];
+    for (let i = 0; i < BLOCK_SIZE; i++) O_KEY_PAD[i] = key[i].charAt(0) ^ O_PAD;
+    const hashSum2 = SHA256(O_KEY_PAD.map(o => String.fromCharCode(o)).join('') + hashSum1);
+    return hashSum2;
 }
 
 console.log(HMAC_SHA256('Suthinan', 'thisIsASecretKey1234'));
